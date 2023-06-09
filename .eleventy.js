871c64f3d0c9c522d1035bb6e6f90dac15ac4365
Archit Chandra <architchandra@gmail.com>
2023-06-09 16:04:12 +0530

    Replace existing homepage with under construction message - first pass

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,11 @@
 module.exports = function(config) {
   config.addPassthroughCopy({ 'src/assets': 'build' });
+  config.addWatchTarget('./styles/');
+
+  config.addFilter('version', function (value) {
+    const now = String(Date.now());
+    return value + '?v' + now;
+  });
   
   return {
     dir: {
